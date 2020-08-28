b91f83de89bb4ef55a73b4a90dccda4813bbda2b
jjongbumeee <danieltiger60@gmail.com>
2020-08-29 00:27:10 +0900

    FE navigation guard test(check '/page2')

--- frontend/src/pages/page2/App.vue
+++ frontend/src/pages/page2/App.vue
@@ -4,14 +4,62 @@
         <p> ID : <input type="text" v-model="id"/> </p>
         <p> PW : <input type="text" v-model="pw"/> </p>
         <button type="submit">로그인</button>
+        <router-link to="/adminAccess">
+          <p>
+            <input type="button" value="관리자 로그인">
+          </p>
+        </router-link>
+        <p><router-view></router-view></p>
     </div>
   </form>
 </template>
 
 <script>
+import Vue from 'vue';
 import axios from 'axios';
 import host from '../../assets/iptable.json';
+import VueRouter from 'vue-router';
+import AdminAccess from './adminAccess.vue';
+Vue.use(VueRouter);
+var vue;
+
+export const router = new VueRouter({
+   routes: [
+    {
+      path: '/',
+      name: 'Main'
+    },
+    {
+      path: '/adminAccess',
+      component: AdminAccess,
+      name: 'adminPage',
+      // meta: {authRequired : true},
+    }
+  ],
+});
+
+router.beforeEach(async function(to, from, next) {
+  console.log(vue);
+  if(to.path == "/adminAccess") {
+    console.log("adminPage access");
+    await axios.get(host.host + '/admin/adminCheck', {
+      params: {
+        token: vue.token
+      }
+    })
+    .then(res=> {
+      console.log(res);
+      next();
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  }
+})
 export default {
+  // components : {
+  //   'admin-access': AdminAccess,
+  // },
   data: function() {
     return  {
       id : "", 
@@ -21,8 +69,9 @@ export default {
   },
   methods : {
     checkAccount : async function() {
-      let vueInstance = this;
-      await axios.post('http://localhost:3000'+`/admin/login`, {
+      const vueInstance = this;
+      vue = vueInstance;
+      await axios.post(host.host+`/admin/login`, {
         id: this.id,
         password: this.pw
       })
@@ -32,8 +81,9 @@ export default {
       .catch(function(err) {
         console.log(err);
       })
-    }
-  }
+    },
+  },
+  router: router,
 }
 </script>
 
