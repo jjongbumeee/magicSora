2d869817678797a55f2deb2412a641925e486641
magicsora Team <danieltiger60@gmail.com>
2020-09-12 15:18:35 +0900

    backend staging

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
     devServer: {
     proxy: {
             '/api': {
-                target: 'http://localhost:3000/api',
+		    target: 'http://203.254.143.175:3000/api',
                 changeOrigin: true,
                 pathRewrite: {
                     '^/api': ''
